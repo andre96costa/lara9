81703bb52dd0b95f0ff09280cd0bbc5978117d5a
Andre Costa <andre2396costa@gmail.com>
2022-04-18 17:33:45 -0300

    Working with blades

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,15 @@
-<h1>Listagem dos usuarios</h1>
+@extends('layouts.app')
+
+@section('title', 'Listagem dos usuários')
+
+@section('content')
+    <h1>Listagem dos usuarios</h1>
+
+    <ul>
+        @foreach ($users as $user)
+            <li>
+            {{ $user->name }} - {{ $user->email }} | <a href="{{ route('users.show', $user->id) }}">Show</a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
